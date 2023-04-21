aec854c6079205414b084282d29d327f64343987
mtn <m.matinjalalitekiyeh@gmail.com>
2023-04-21 11:18:29 -0400

    impl an example for solid except dependency inversion principle.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#define SEPERATOR std::cout << "******************\n"
+int ext = 0;
+void set_exit_value(int value) {
+    SEPERATOR;
+    ext = value;
+}
+#include "single_responsibility.hpp"
+#include "solid/open_close/open_close.hpp"
+#include "solid/liskov/liskov.hpp"
+#include "solid/interface/interface.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    SINGLE_RESPONSIBILITY::test();
+    OPEN_CLOSE::test();
+    LISKOV::test();
+    INTERFACE::test();
+    return ext;
 }
