5d3a346f202c3178ffb7ab9f5f9568c3d1e26b6a
mtn <m.matinjalalitekiyeh@gmail.com>
2023-04-21 08:54:29 -0400

    add struct for single responsibility in solid desing pattern

---
+++ solid/single_responsiblity.hpp
@@ -0,0 +1,12 @@
+#ifndef SINGLE_RESPONSIBILITY_HPP
+#define SINGLE_RESPONSIBILITY_HPP
+#include <iostream>
+#include <string>
+
+struct Entery {
+    std::string title;
+};
+
+#endif // SINGLE_RESPONSIBILITY_HPP
+ 
+
